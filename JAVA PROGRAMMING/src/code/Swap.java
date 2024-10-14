eddfcfc4b65d3eb6b450bb3ef9bc19b4b71f1530
Soumy24 <soumyashrivas.2000@gmail.com>
2024-10-14 13:00:04 +0530

    Code Completed

---
+++ JAVA PROGRAMMING/src/code/Swap.java
@@ -0,0 +1,13 @@
+package code;
+
+public class Swap {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a is " + a + " and b is " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After Swapping a is " + a + " and b is " + b);
+    }
+}
